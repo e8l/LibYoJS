5bdb78e93966144f86634d785902dbe14f60e9de
e8l <tou_roku_09@yahoo.co.jp>
2015-03-01 22:04:36 +0900

    add document

---
+++ doc/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "LibYo",
+        "Yo"
+    ],
+    "modules": [
+        "LibYo"
+    ],
+    "allModules": [
+        {
+            "displayName": "LibYo",
+            "name": "LibYo"
+        }
+    ]
+} };
+});
